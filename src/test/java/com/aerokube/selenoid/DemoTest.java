b3e02ae4d3297f88e28d2ff230e785196832d44f
megorov <Zx10242048>
2021-02-16 17:00:23 +0400

    DemoTest

--- src/test/java/com/aerokube/selenoid/DemoTest.java
+++ src/test/java/com/aerokube/selenoid/DemoTest.java
@@ -36,8 +36,8 @@ public class DemoTest {
             //WebElement input = driver.findElement(By.cssSelector("input#search_form_input_homepage"));
             WebElement input = driver.findElement(By.xpath("//*[@id=\"text\"]"));
 
-            input.sendKeys(Keys.chord("selenium", Keys.ENTER));
-            Thread.sleep(10);
+            input.sendKeys(Keys.chord("АО Neoflex", Keys.ENTER));
+            Thread.sleep(1000000);
         } finally {
             takeScreenshot(driver);
         }
